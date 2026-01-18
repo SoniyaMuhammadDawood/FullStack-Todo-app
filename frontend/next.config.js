1eb19d7f02a6622d6997fbabc80ae9ec0d78f134
SoniyaMuhammadDawood <soniyadawood138@gmail.com>
2026-01-19 01:32:25 +0500

    error

--- frontend/next.config.js
+++ frontend/next.config.js
@@ -3,9 +3,6 @@ const nextConfig = {
   env: {
     NEXT_PUBLIC_API_BASE_URL: process.env.NEXT_PUBLIC_API_BASE_URL || 'http://localhost:8000',
   },
- experimental: {
-    turbo: false, // Disable Turbopack to use traditional webpack-based build
-  },
 };
 
 module.exports = nextConfig;
